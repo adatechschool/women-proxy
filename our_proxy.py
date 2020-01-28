c97ac6b857199041fb3e9bbfe0a1359f3840d5b8
Leirda01 <adrieldj@orange.fr>
2020-01-28 11:23:53 +0100

    Gère correctement le contrôle C

--- our_proxy.py
+++ our_proxy.py
@@ -3,9 +3,25 @@
 import socket
 import threading
 import requests
+import signal
+import sys
 
 from http.server import BaseHTTPRequestHandler
 from io import BytesIO
+
+def quit_on_ctrl_c(sig, frame):
+    print()
+    sys.exit(0)
+signal.signal(signal.SIGINT, quit_on_ctrl_c)
+
+def handle_client_connection(client_socket):
+    raw_request = client_socket.recv(1024)
+    request = HTTPRequest(raw_request)
+    if request.error_code:
+        print (f'Error : {request.error_code} : {request.error_message}')
+    else:
+        print (f'Received {request.command} {request.path}')
+    client_socket.close()
 
 class HTTPRequest(BaseHTTPRequestHandler):
     def __init__(self, request_text):
@@ -20,18 +36,10 @@ class HTTPRequest(BaseHTTPRequestHandler):
 
 bind_ip = '0.0.0.0'
 bind_port = 9999
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
 server.listen(5)
-
 print (f'Listening on {bind_ip}:{bind_port}')
-
-def handle_client_connection(client_socket):
-    raw_request = client_socket.recv(1024)
-    request = HTTPRequest(raw_request)
-    print (f'Received {request.path} {request.command}')
-    client_socket.close()
 
 while True:
     client_sock, address = server.accept()
@@ -41,4 +49,3 @@ while True:
         args=(client_sock,)
     )
     client_handler.start()
-
