940b75275150468cc563ea926a944fcf81c5eed5
Mohamed <mrmoha94@gmail.com>
2020-01-29 15:50:46 +0100

    Rend le code plus boooo!
    Signed-off-by: Mohamed <mrmoha94@gmail.com>

--- proxy.py
+++ proxy.py
@@ -7,18 +7,17 @@ from handle_requests import *
 
 class proxy(socket):
     def __init__(self, ip, port, backlog = 0): 
-        super().__init__(AF_INET, SOCK_STREAM)
         self.bind_ip = ip
         self.bind_port = port
 
+        super().__init__(AF_INET, SOCK_STREAM)
         super().bind((self.bind_ip, self.bind_port))
         super().listen(backlog)
 
-        print (f'Listening on {self.bind_ip}:{self.bind_port}')
+        print(f'Listening on {self.bind_ip}:{self.bind_port}')
 
     def handle_client_connection(client_socket):
-        raw_request = client_socket.recv(1024)
-        request = HTTPRequest(raw_request)
+        request = HTTPRequest(client_socket.recv(1024))
         if request.error_code:
             print(f'Error : {request.error_code} : {request.error_message}')
         else:
